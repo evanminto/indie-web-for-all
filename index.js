adae9e539661fa3eab77cfa6c89cfc7d53a5f2bd
Evan Minto <evan.minto@gmail.com>
2016-11-05 10:00:00 -0700

    Fixes port for Heroku

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
